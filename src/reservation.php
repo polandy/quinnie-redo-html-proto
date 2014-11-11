57fa27832f6104d6e2cd121f99f7b5d7323838ed
flec <mfuellemann@gmail.com>
2014-11-11 21:21:58 +0100

    update reservation

---
+++ src/reservation.php
@@ -0,0 +1,19 @@
+<?php
+include "layout/header.php"; ?>
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Reservation</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-lg-8">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+<?php include "layout/footer.php";
