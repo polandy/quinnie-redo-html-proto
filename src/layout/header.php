22940de2158ba6536492f3fefcd303fbf4464c36
Andy <andy.pollari@gmail.com>
2014-11-11 20:44:59 +0100

    added sites

--- src/layout/header.php
+++ src/layout/header.php
@@ -1,8 +1,16 @@
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-
-
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../js/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="../js/plugins.js"></script>
+        <script src="../js/main.js"></script>
+        <link rel="stylesheet" href="../css/main.css">
     </head>
 
     <body>
         <div>
+        hhu
