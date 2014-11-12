0cdfa910e0fab67ffb6456ac06af6581979067d8
flec <mfuellemann@gmail.com>
2014-11-12 19:11:57 +0100

    added chairs for reservation

--- src/reservation.php
+++ src/reservation.php
@@ -1,5 +1,9 @@
 <?php
-include "layout/header.php"; ?>
+include "layout/header.php";
+
+
+
+?>
     <div class="row">
         <div class="col-sm-12">
             <h1>Reservation</h1>
@@ -9,6 +13,24 @@ include "layout/header.php"; ?>
         <div class="col-sm-12 col-lg-8">
             <div class="panel panel-default">
                 <div class="panel-body">
+                    <div id="res-canvas">Leinwand</div>
+                    <div class="res-chairs">
+                    <?php
+                    for ($i = 0; $i < 10; $i++) {
+                        ?>
+                        <div class="res-chair-row">
+                            <?php
+                            for ($j = 0; $j < 15; $j++) {
+                                ?>
+                                <div class="res-chair"></div>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                    </div>
                 </div>
             </div>
         </div>
