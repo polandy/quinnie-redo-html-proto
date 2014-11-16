e4323da7b3eb34084b6cd7ea6690c17f6bd4c9f8
joschi03 <joschi0391@gmail.com>
2014-11-16 20:56:58 +0100

    kontakt

---
+++ src/contact.php
@@ -0,0 +1,53 @@
+<?php
+include "layout/header.php"; ?>
+
+<h1>Quinnie über sich</h1>
+
+<div class="row">
+    <div class="col-sm-12 col-md-8">
+		<div>
+			<br />
+			<p>
+				Die Berner Kinokette Quinnie Cinemas vereinigt einige der wichtigsten Berner
+	            Arthouse Kinos organisatorisch unter einem Dach. Gezeigt werden Spielfilme,
+	            Dokumentarfilme, Schweizer Filme und Studiofilme für ein anspruchsvolles Kinopublikum.
+	        </p>
+	        <p>
+		        Wir bieten Kinoerlebnisse auf höchstem Niveau. Einein
+	            Bild und Ton perfekte und pannenfreie Filmvorführung
+	            gehört genau so dazu wie ein umfassender Kundenservice.
+	            Quinnie Cinemas steht für Qualität von der Filmauswahl
+	            über eine perfekte Projektion bis zum massgeschneiderten Rahmenevent.
+	        </p>
+		</div>
+	</div>
+    <div class="col-sm-12 col-md-4">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<h3>Jobben bei Quinnie</h3>
+				<p>
+					Qunnie ist immer auf der Suche nach billigen Arbeitskräften, welche sich ausbeuten lassen. <br />
+					Willst du zu dieser illustren Runde gehören? <br />
+					dann ruf uns an:<br />
+	                031 301 91 91
+				</p>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<h3>Qunnie kontaktieren</h3>
+				<p>
+					Quinnie Cinemas<br />
+	                Seilerstrasse 4<br />
+	                CH - 3011 Bern<br />
+	                eMail: info@quinnie.ch<br />
+	                Home: www.quinnie.ch<br />
+	                Telefon: + 41 31 386 17 10<br />
+	                Fax: + 41 31 386 17 11<br />
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include "layout/footer.php";
